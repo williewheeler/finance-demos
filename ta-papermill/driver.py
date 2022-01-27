52a85e9a7e026b7c634ff34063a10408171f32b1
Willie Wheeler <willie.wheeler@gmail.com>
2022-01-26 22:42:48 -0800

    Added technical analysis based on Papermill

---
+++ ta-papermill/driver.py
@@ -0,0 +1,22 @@
+import os
+import papermill as pm
+
+ticker_symbols = [
+    "AAPL",
+    "AMZN",
+    "FB",
+    "GOOG",
+    "MSFT",
+    "O",
+    "PLTR",
+    "SBUX",
+]
+
+out_dir = "./out"
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+
+for ts in ticker_symbols:
+    input = "ta.ipynb"
+    output = f"{out_dir}/ta-{ts}.ipynb"
+    pm.execute_notebook(input, output, parameters={ "ticker_symbol": ts })
