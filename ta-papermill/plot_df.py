0b672b10f557b3d0c9f5b44c88250570ef5649dd
Willie Wheeler <willie.wheeler@gmail.com>
2022-01-28 02:37:44 -0800

    Stochastic oscillator cleanup

--- ta-papermill/plot_df.py
+++ ta-papermill/plot_df.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import mplfinance as mpf
 
+from matplotlib.lines import Line2D
+
+
 today = datetime.datetime.now()
 
 date_pattern = "%Y-%m-%d"
@@ -55,3 +58,30 @@ def plot_bollinger(df, symbol, rng):
         title=f"{symbol} with Bollinger Bands, {rng}",
         style=["-", "--", "-", "-"],
         figsize=(6, 4))
+
+
+def plot_stochastic_oscillator(df, symbol, rng, periods=14):
+    start = date_ranges[rng]
+    end = today_str
+    temp_df = df[start:end]
+    
+    fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, tight_layout=True, figsize=(6, 6))
+
+    ax[0].set_title(f"{symbol} price, {rng}")
+    ax[0].plot(temp_df["Close"], color="tab:blue")
+
+    ax[1].set_title(f"{symbol} Stochastic Oscillator ({periods}-day period), {rng}")
+    ax[1].set_ylim(-10, 110)
+    ax[1].plot(temp_df["%K"], color="tab:blue") # fast
+    ax[1].plot(temp_df["%D"], color="tab:orange") # slow
+
+    ax[1].axhline(80, color="tab:red", ls="--")
+    ax[1].axhline(20, color="tab:green", ls="--")
+
+    custom_lines = [
+        Line2D([0], [0], color="tab:blue", lw=4),
+        Line2D([0], [0], color="tab:orange", lw=4),
+        Line2D([0], [0], color="tab:red", lw=4),
+        Line2D([0], [0], color="tab:green", lw=4),
+    ]
+    ax[1].legend(custom_lines, ["%K", "%D", "Overbought", "Oversold"], loc="best")
--- ta-papermill/prepare_df.py
+++ ta-papermill/prepare_df.py
@@ -10,3 +10,20 @@ def add_bollinger_bands(df):
     copy["LowerBB"] = mean - two_sigmas
     
     return copy
+
+
+def add_stochastic_oscillator(df, periods=14):
+    copy = df.copy()
+    
+    high_roll = copy["High"].rolling(periods).max()
+    low_roll = copy["Low"].rolling(periods).min()
+    
+    # Fast stochastic indicator
+    num = copy["Close"] - low_roll
+    denom = high_roll - low_roll
+    copy["%K"] = (num / denom) * 100
+    
+    # Slow stochastic indicator
+    copy["%D"] = copy["%K"].rolling(3).mean()
+    
+    return copy
